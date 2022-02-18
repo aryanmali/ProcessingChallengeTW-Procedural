f0a08eb9083fa014c9aec17f0b345c27969a4876
Aryan Mali <aryanrvmali@gmail.com>
2022-02-18 12:14:55 +0530

    Initial Commit

---
+++ src/main/java/ProcessingPkgFourBalls.java
@@ -0,0 +1,26 @@
+import processing.core.PApplet;
+
+public class ProcessingPkgFourBalls extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 10;
+    int xCoordinate = 0;
+
+    public static void main(String[] args) {
+        PApplet.main("ProcessingPkgFourBalls", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);git
+    }
+
+    @Override
+    public void draw() {
+        ellipse(xCoordinate++,HEIGHT/5, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 2,HEIGHT/5 * 2, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 3,HEIGHT/5 * 3, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 4,HEIGHT/5 * 4, DIAMETER, DIAMETER);
+    }
+}
